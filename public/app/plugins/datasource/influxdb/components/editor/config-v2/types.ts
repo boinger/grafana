f03f5aa9547ed420537926b4802fdd930b9d768e
alyssabull <alyssabull@gmail.com>
2026-04-09 22:16:31 -0600

    Remove duplicate type

--- public/app/plugins/datasource/influxdb/components/editor/config-v2/types.ts
+++ public/app/plugins/datasource/influxdb/components/editor/config-v2/types.ts
@@ -1,4 +1,4 @@
-import { type DataSourceConfigValidationAPI, type DataSourcePluginOptionsEditorProps } from '@grafana/data';
+import { type DataSourcePluginOptionsEditorProps } from '@grafana/data';
 
 import { type InfluxOptions, type InfluxSecureJsonData } from '../../../types';
 
@@ -19,6 +19,4 @@ type InfluxSecureBasicAuthData = {
 export type Props = DataSourcePluginOptionsEditorProps<
   InfluxOptions & InfluxBasicAuthData,
   InfluxSecureJsonData & InfluxSecureBasicAuthData
-> & {
-  validation?: DataSourceConfigValidationAPI;
-};
+>;
